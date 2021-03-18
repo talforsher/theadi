33bffaa63b74e97c728fdeb0bba7dc395d7286c8
adikrief <44708302+adikrief@users.noreply.github.com>
2021-03-18 08:15:00 +0200

    Auto Deploy

--- edit.js
+++ edit.js
@@ -187,11 +187,11 @@ function save() {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Buit with Tal Forsher CMS">
-    <meta name="keywords" content="Tal Forsher">
+    <meta name="description" content="הסדנה מעשית ורב גילאית. כל משתתף יוצא עם מארז אישי מפנק עם כל התוצרים הטעימים שהכין בסדנה.
+    <meta name="keywords" content="העדי שבמטבח">
     <meta name="robots" content="index, follow">
-    <link rel="icon" href="dksimulations.jpg">
-    <title>Tenerife Project</title>
+    <link rel="icon" href="img372.webp">
+    <title>סדנאות שוקולד בפסח ברמת הגולן | העדי שבמטבח</title>
     <style>
         body {
             margin: 0;
